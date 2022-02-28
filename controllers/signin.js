cc590f5be73e56843a7a0c4b841e112d80474a75
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2022-02-28 18:53:31 +0300

    adding project files

---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignIn = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("invalid form submission");
+  }
+  db("users")
+    .join("login", "users.email", "=", "login.email")
+    .select("*")
+    .where("users.email", email)
+    .then(data => {
+      if (data.length !== 0 && bcrypt.compareSync(password, data[0].hash)) {
+        // res.json({
+        //   id: data[0].id,
+        //   name: data[0].name,
+        //   email: data[0].email,
+        //   entries: data[0].entries,
+        //   joined: data[0].joined,
+        // });
+        delete data[0].hash;
+        return res.json(data[0]);
+      } else {
+        return res.status(400).json("invalid user credentials");
+      }
+    })
+    .catch(err => res.status(400).json("error signing in"));
+};
+
+module.exports = {
+  handleSignIn,
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const app = express();
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const image = require("./controllers/image");
+const profile = require("./controllers/profile");
+
+app.use(express.json());
+app.use(cors());
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "burakkosova",
+    password: "",
+    database: "smart-brain",
+  },
+});
+
+// app.get("/", (req, res) => {
+//   db.select()
+//     .table("users")
+//     .then(data => res.json(data));
+// });
+
+app.post("/signin", (req, res) => signin.handleSignIn(req, res, db, bcrypt));
+
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+
+app.put("/image", (req, res) => image.handleImage(req, res, db));
+
+app.listen(3000, () => console.log("app is running on port 3000"));
